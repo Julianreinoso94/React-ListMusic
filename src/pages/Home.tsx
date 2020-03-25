1deeeab7d7280e2f5c70ed3bc6fbf797a562cb4b
julianreinoso94 <julianreinoso94@gmail.com>
2020-03-25 13:24:10 -0300

    Add list to proyect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonText } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+
+const arr =[
+  {
+   name:'Finn',
+   desc:'this guy rocks!' 
+  },
+  {
+    name:'Han',
+    desc:'Trust me, I am programmer'
+  },
+  {
+    name: 'Rey',
+    desc:'i am done with it'
+  }
+]
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Hello how are you</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="">pararapa
+       <IonList>
+          {arr
+          .map(element=>(
+            <IonItem key={element.name}>
+              <IonText>List Item {element.desc}</IonText>
+            </IonItem>
+          )) }
+       </IonList>
       </IonContent>
     </IonPage>
   );
